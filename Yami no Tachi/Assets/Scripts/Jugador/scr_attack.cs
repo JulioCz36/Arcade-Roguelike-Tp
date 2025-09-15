b72a93204090a780e6a1934f012ad31850374909
Julio Rodriguez <109080025+JulioCz36@users.noreply.github.com>
2025-09-15 12:48:26 -0300

    Ataque del personaje

--- Yami no Tachi/Assets/Scripts/Jugador/scr_attack.cs
+++ Yami no Tachi/Assets/Scripts/Jugador/scr_attack.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
 
-public class scr_attack : MonoBehaviour {
+public class scr_attack : MonoBehaviour
+{
 
     private Animator mi_animator;
+    private bool isAttacking = false;
+
+    [Header("Efecto de corte")]
+    public GameObject slashPrefab;  
+    public Transform attackPoint;
 
     private void OnEnable(){
         mi_animator = GetComponent<Animator>();
@@ -10,14 +16,26 @@ public class scr_attack : MonoBehaviour {
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && !isAttacking)
         {
             Atacar();
         }
     }
 
-    void Atacar(){
+    void Atacar()
+    {
+        isAttacking = true;
         mi_animator.SetTrigger("attack");
+
+        GameObject slash = Instantiate(slashPrefab, attackPoint.position, attackPoint.rotation, attackPoint);
+
+        float duracion = mi_animator.GetCurrentAnimatorStateInfo(0).length;
+        Destroy(slash, duracion);
+    }
+
+    public void TerminarAtaque()
+    {
+        isAttacking = false;
     }
 
 }
